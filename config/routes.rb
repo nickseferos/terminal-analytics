4eadc44e9271c039bddd20bd2daa2b645d5c3ab5
nickseferos <nick.seferos@gmail.com>
2020-01-21 17:35:33 -0800

    Modufy root to be dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,15 @@ Rails.application.routes.draw do
   resources :shifts
   resources :port_calls
   resources :vessels
+
+  unauthenticated :user do
+    root 'public#index'
+    get 'about', to: 'public#about'
+    get 'contact', to: 'public#contact'
+  end
+
+  authenticated :user do
+    root 'vessels#index', as: "authenticated_root"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
